d54f8039ec4d630d711f18034dd423a2caf1d68e
gururajops <gururajops@gmail.com>
2021-09-06 22:55:57 +0530

    added inital coomit

---
+++ src/main/java/com/mydemocompany/first_demo/App.java
@@ -0,0 +1,13 @@
+package com.mydemocompany.first_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
